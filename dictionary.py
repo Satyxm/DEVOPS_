710b718acfe5e571defb19a3e7dad7da25356982
Satyam Singh <97077594+Satyxm@users.noreply.github.com>
2022-12-19 17:07:21 +0530

    Dictionary in python.

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+print("This is a Dictionary")
+
+this_dictionary = {
+     "key" : "value_pairs",
+     "First" : "1",
+     "Second" : "2"
+ }
+
+print(this_dictionary)
+print(this_dictionary["First"])
