0c03208537cf9343c9cdd6f2b4ed345c1c5c2f4a
Satyam Singh <singhsatyam4702@gmail.com>
2022-12-26 10:16:24 +0530

    Dictionary Comprehension file added.

---
+++ dcomp.py
@@ -0,0 +1,3 @@
+weather = {'New york' : 'foggy', 'Chicago' : 'sunny', 'Brooklyn' : 'rainy', 'Boston' : 'sunny'}
+sunny_weather = {key: value for (key, value) in weather.items() if value == "sunny"}
+print(sunny_weather)
